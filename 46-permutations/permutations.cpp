4374f6aa62b4b2c75e1655491677b4b472d056db
Chetanya Bhargav <183283890+bhargavchetanya@users.noreply.github.com>
2026-09-02 17:49:08 +0530

    Time: 2 ms (36.14%) | Memory: 10.4 MB (98.29%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -3,7 +3,6 @@ public:
     void f(vector<int>&temp,vector<bool>&used,vector<int>& nums,vector<vector<int>>&ans){
         if(temp.size()==nums.size()){
             ans.push_back(temp);
-            return;
         }
         for(int i=0;i<nums.size();i++){
             if(used[i]==true)continue;
