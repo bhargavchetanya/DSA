9d3486d42830ca05c6f15e7def485cec10f87545
Chetanya Bhargav <183283890+bhargavchetanya@users.noreply.github.com>
2026-09-03 13:30:09 +0530

    Time: 6 ms (52.36%) | Memory: 165.9 MB (31.94%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -5,24 +5,14 @@ public:
         int smallest=INT_MAX;
         int n=nums1.size();
         for(int i=0;i<n;i++){
-            if(nums1[i]%2==0){
-                e++;
-            }
-            else{
-                o++;
-            }
+            if(nums1[i]%2==0)e++;
+            else o++;
             smallest=min(smallest,nums1[i]);
         }
         if(smallest%2==0){
-            //nums2->even
             if(o==0)return true;
-            else return false;
-
         }
-        else{
-            //nums2->odd
-            return true;
-        }
+        else return true;
         return false;
     }
 };
